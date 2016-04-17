cc6c23181846e25e6424720a3a00d713a3a91d65
ilavs <lamourskij@gmail.com>
2016-04-17 04:16:56 +0300

    Lavs

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+$(document).ready(function() {
+  $('a[href="#"]').click(function (e){
+    e.preventDefault();
+  });
+
+  // menu
+
+  $('.burger').click(function() {
+  	$('.navbar, .burger').toggleClass('active');
+    $('.backdrop').toggle();
+  });
+
+  $('.backdrop').click(function(event) {
+     $('.navbar, .burger').removeClass('active');
+     $(this).fadeOut();
+  });
+
+  // testimonial slider
+  $('.testimonial-slider').owlCarousel({
+      loop: true,
+      items:1,
+      navigation: true 
+  });
+
+});
